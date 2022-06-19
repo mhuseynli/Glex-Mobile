c3449136096a56d91eaac919e6b2fbede46b2a1a
mhuseynli <muradhuseynli570@gmail.com>
2022-06-20 00:21:16 +0400

    nem ala

--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -1,5 +1,104 @@
 <template>
+  <q-list separator bordered class="bg-white menu">
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-path="url(#clip0_22_2568)">
+            <path
+              d="M16.7925 9.10823L6.93091 3.42139"
+              stroke="#585C5E"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M21.7232 16.3401V7.57421C21.7228 7.18991 21.6213 6.81247 21.429 6.47976C21.2367 6.14704 20.9602 5.87075 20.6274 5.6786L12.9573 1.29568C12.6242 1.10333 12.2463 1.00208 11.8616 1.00208C11.4769 1.00208 11.099 1.10333 10.7658 1.29568L3.09573 5.6786C2.76292 5.87075 2.48648 6.14704 2.29416 6.47976C2.10184 6.81247 2.00039 7.18991 2 7.57421V16.3401C2.00039 16.7244 2.10184 17.1018 2.29416 17.4345C2.48648 17.7672 2.76292 18.0435 3.09573 18.2357L10.7658 22.6186C11.099 22.8109 11.4769 22.9122 11.8616 22.9122C12.2463 22.9122 12.6242 22.8109 12.9573 22.6186L20.6274 18.2357C20.9602 18.0435 21.2367 17.7672 21.429 17.4345C21.6213 17.1018 21.7228 16.7244 21.7232 16.3401Z"
+              stroke="#585C5E"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M2.2959 6.43463L11.8616 11.9681L21.4274 6.43463"
+              stroke="#585C5E"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M11.8616 23.0021V11.9571"
+              stroke="#585C5E"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_22_2568">
+              <rect width="24" height="24" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </q-item-section>
 
+      <q-item-section class="menu__item-label">Bağlamalarım</q-item-section>
+    </q-item>
+    <q-item clickable v-ripple>
+      <q-item-section avatar>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-path="url(#clip0_22_2568)">
+            <path
+              d="M16.7925 9.10823L6.93091 3.42139"
+              stroke="#585C5E"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M21.7232 16.3401V7.57421C21.7228 7.18991 21.6213 6.81247 21.429 6.47976C21.2367 6.14704 20.9602 5.87075 20.6274 5.6786L12.9573 1.29568C12.6242 1.10333 12.2463 1.00208 11.8616 1.00208C11.4769 1.00208 11.099 1.10333 10.7658 1.29568L3.09573 5.6786C2.76292 5.87075 2.48648 6.14704 2.29416 6.47976C2.10184 6.81247 2.00039 7.18991 2 7.57421V16.3401C2.00039 16.7244 2.10184 17.1018 2.29416 17.4345C2.48648 17.7672 2.76292 18.0435 3.09573 18.2357L10.7658 22.6186C11.099 22.8109 11.4769 22.9122 11.8616 22.9122C12.2463 22.9122 12.6242 22.8109 12.9573 22.6186L20.6274 18.2357C20.9602 18.0435 21.2367 17.7672 21.429 17.4345C21.6213 17.1018 21.7228 16.7244 21.7232 16.3401Z"
+              stroke="#585C5E"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M2.2959 6.43463L11.8616 11.9681L21.4274 6.43463"
+              stroke="#585C5E"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M11.8616 23.0021V11.9571"
+              stroke="#585C5E"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_22_2568">
+              <rect width="24" height="24" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </q-item-section>
+
+      <q-item-section>Bağlamalarım</q-item-section>
+    </q-item>
+  </q-list>
 </template>
 
 <script>
